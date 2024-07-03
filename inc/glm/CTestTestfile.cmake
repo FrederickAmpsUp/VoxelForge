9cbff40e6994ee0d679eb334863d40c35620948e
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2024-07-03 01:33:24 -0400

    Start over, with FGLW this time

--- inc/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frederickampsup/Downloads/glm/glm
-# Build directory: /home/frederickampsup/Downloads/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

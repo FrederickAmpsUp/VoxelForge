7f408208250a71a4d7816e2e0fe0d4eb5698ae44
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-01-19 18:34:45 -0500

    Add device score function

--- inc/voxelforge/util.hpp
+++ inc/voxelforge/util.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <vector>
+#include <voxelforge/types.hpp>
+#include <vulkan/vulkan.h>
 
 namespace vf {
 
 const std::vector<const char *>& get_required_instance_extensions();
+i64 score_device(VkPhysicalDevice& dev);
 }

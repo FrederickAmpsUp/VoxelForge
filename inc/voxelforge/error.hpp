ad2a18c8e30e821fa9278b113303d6b86a346605
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-01-19 17:13:35 -0500

    Update namespace to 'vf'

--- inc/voxelforge/error.hpp
+++ inc/voxelforge/error.hpp
@@ -4,7 +4,7 @@
 #include <optional>
 #include <string>
 
-namespace vforge {
+namespace vf {
 
 struct Error {
 	std::string message;
--- inc/voxelforge/util.hpp
+++ inc/voxelforge/util.hpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-namespace vforge {
+namespace vf {
 
 const std::vector<const char *>& get_required_instance_extensions();
 }

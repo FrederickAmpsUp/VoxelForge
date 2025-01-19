603fcd030204afb0bc1dd8759cca75b47bde445d
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-01-19 18:35:46 -0500

    Add device score function

--- src/util.cpp
+++ src/util.cpp
@@ -7,3 +7,7 @@ static const std::vector<const char *> instance_extensions = {
 const std::vector<const char *>& vf::get_required_instance_extensions() {
 	return instance_extensions;
 }
+
+i64 score_device(VkPhysicalDevice& dev) {
+	return 1; // no requirements atm
+}
